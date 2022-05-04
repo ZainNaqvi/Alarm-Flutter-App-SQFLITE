b7424dfdcfe0c2c4efa04cfff2e52a33e2bbc693
Zain <syedzainnaqvi3324@gmail.com>
2022-05-04 17:56:48 +0500

    home screen alarm screen is completed

--- lib/screens/alarm.dart
+++ lib/screens/alarm.dart
@@ -1,5 +1,7 @@
 import 'package:digital_alarm_app/colors/theme_data.dart';
 import 'package:digital_alarm_app/data.dart';
+import 'package:digital_alarm_app/model/alarm_info.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AlarmScreen extends StatelessWidget {
@@ -23,85 +25,120 @@ class AlarmScreen extends StatelessWidget {
         Expanded(
           child: ListView(
             children: alarms
-                .map(
-                  (e) => Container(
-                    margin: EdgeInsets.only(bottom: 32),
-                    padding: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      gradient: LinearGradient(
-                        colors: GradientColors.sea,
-                        begin: Alignment.centerRight,
-                        end: Alignment.centerLeft,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.red.withOpacity(0.4),
-                          blurRadius: 8,
-                          spreadRadius: 2,
-                          offset: Offset(4, 4),
-                        ),
-                      ],
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(24),
-                      ),
+                .map<Widget>(
+              (e) => Container(
+                margin: EdgeInsets.only(bottom: 32),
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  gradient: LinearGradient(
+                    colors: GradientColors.fire,
+                    begin: Alignment.centerRight,
+                    end: Alignment.centerLeft,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: GradientColors.fire.last.withOpacity(0.4),
+                      blurRadius: 8,
+                      spreadRadius: 2,
+                      offset: Offset(4, 4),
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  ],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(24),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.label,
-                                  color: Colors.white,
-                                  size: 24,
-                                ),
-                                SizedBox(
-                                  width: 8,
-                                ),
-                                Text(
-                                  "Office",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ],
+                            Icon(
+                              Icons.label,
+                              color: Colors.white,
+                              size: 24,
                             ),
-                            Switch(value: true, onChanged: (bool value) {}),
-                          ],
-                        ),
-                        Text(
-                          "Mon - Fri",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
+                            SizedBox(
+                              width: 8,
+                            ),
                             Text(
-                              "07:00 AM",
+                              alarms.map((e) => e.description).first.toString(),
                               style: TextStyle(
                                 color: Colors.white,
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                            Icon(
-                              Icons.keyboard_arrow_down_outlined,
-                              color: Colors.white,
-                              size: 36,
-                            )
                           ],
+                        ),
+                        Switch(
+                          activeColor: Colors.white,
+                          value: true,
+                          onChanged: (bool value) {},
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "Mon - Fri",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "07:00 AM",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color: Colors.white,
+                          size: 36,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+                .followedBy([
+              DottedBorder(
+                strokeWidth: 2,
+                dashPattern: [8, 4],
+                color: CustomColors.clockBG,
+                borderType: BorderType.RRect,
+                radius: Radius.circular(32),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: CustomColors.clockBG,
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  child: FlatButton(
+                    padding: EdgeInsets.all(24),
+                    onPressed: () {},
+                    child: Column(
+                      children: [
+                        Image.asset('add_alarm.png'),
+                        SizedBox(
+                          height: 14,
+                        ),
+                        Text(
+                          "Add Alarm",
+                          style: TextStyle(color: Colors.white, fontSize: 14),
                         ),
                       ],
                     ),
                   ),
-                )
-                .toList(),
+                ),
+              )
+            ]).toList(),
           ),
         ),
       ]),
